1019dc7ae06f697b42b88abb53c0badc6a6e1788
CRONOXT <59267988+CRONOXT@users.noreply.github.com>
2022-08-11 12:10:40 -0400

    Se agregaron comentarios
    Se agregaron comentarios en varias partes del codigo para mayor compresion

--- src/busqueda/IBusqueda.ts
+++ src/busqueda/IBusqueda.ts
@@ -3,7 +3,9 @@ import { CreateShoesDto } from 'src/shoes/dto/create-shoes.dto';
 import { Shoes } from 'src/shoes/model/shoes';
 import { CreateShoesModelDto } from 'src/shoes_model/dto/create-shoes_model';
 import { CreateStoreDto } from 'src/store/dto/create-storedto';
-
+//INTERFACE USADA PARA PODER APLICAR EL PATRON REPOSITORY
+//AQUI SE DEFINEN TODAS LAS FUNCIONES QUE SE VAN A CREAR EN
+//SHOES REPOSITORY PARA LUEGO LLAMARLAS DESDE EL SERVICIO
 export interface IBusqueda {
   busquedaPorMarca(filtro: CreateBrandDto): Promise<Shoes[]>;
   busqueda(): Promise<Shoes[]>;
